ac3cfb2922db9f2668484c1757b26ac569149b0a
Nick Sehy <nisehy@microsoft.com>
2020-07-28 20:51:51 -0700

    getting convos to work

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const http = require('http');
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const accountSid = ''; //PUT TWILIO CREDENTIALS HERE
-const authToken = '';  //PUT TWILIO CREDENTIALS HERE
+const accountSid = 'AC0390e3d8181b603d0097df2843b0b23b'; //PUT TWILIO CREDENTIALS HERE
+const authToken = 'e7541f3f894835580355617faa6c4ee1';  //PUT TWILIO CREDENTIALS HERE
 const client = require('twilio')(accountSid, authToken);
 
 var multer  = require('multer')
@@ -16,40 +17,44 @@ app.use(bodyParser.urlencoded({extended: true}));
 const MessagingResponse =
   require("twilio").twiml.MessagingResponse;
 
-  app.post("/",  upload.single('myfile'), function (req, res, next){
-    console.log(req.body);
-    console.log("Break")
-    console.log(req.file);
+app.post("/",  upload.single('myfile'), function (req, res, next){
+  console.log(req.body);
+  console.log("Break")
+  console.log(req.file);
 
-    //make sure that excel sheet is in the correct format, which is an excel sheet of rows? instead lookup file?
-    //if it's in xlsx, convert it to csv
-    //for each line,
-          //make sure line is in correct format. if it's not, print a message saying it's not.
-          //send the message.
-    //get req.body.
-    console.log(req.body.message);
-    client.messages
-      .create({
-         body: req.body.message,
-         from: '+1', //PUT TWILIO PHONE NUMBER HERE
-         to: '+1'   //PUT NASTY NICKS PHONE NUMBER HERE
-       })
-      .then(message => console.log(message.sid));
+  //make sure that excel sheet is in the correct format, which is an excel sheet of rows? instead lookup file?
+  //if it's in xlsx, convert it to csv
+  //for each line,
+        //make sure line is in correct format. if it's not, print a message saying it's not.
+        //send the message.
+  //get req.body.
+  console.log(req.body.message);
+  client.messages
+    .create({
+        body: req.body.message,
+        from: '+15863156741', //PUT TWILIO PHONE NUMBER HERE
+        to: '+12483038027'   //PUT NASTY NICKS PHONE NUMBER HERE
+      })
+    .then(message => console.log(message.sid));
+});
 
+app.post('/', (req, res) => {
+  const twiml = new MessagingResponse();
 
-  /*  const twiml = new MessagingResponse();
-    twiml.message(req.body.message);
-    res.end(twiml.toString());
+  const message = twiml.message();
+  message.body('The Robots are coming! Head for the hills!');
 
-    console.log("Message: " + req.body.message)
-    res.redirect(__dirname + "/index.html");*/
-  });
+  console.log("found")
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
 
 app.get("/", function(req, res)
 {
   res.sendFile(__dirname + "/index.html")
 });
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000");
-})
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
+});
